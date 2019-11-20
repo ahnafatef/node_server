7aa9f57348d5f50f95d24bf316c1f2162cfb90f5
ahnafatef <ahnafatefkhan@gmail.com>
2019-11-20 09:26:01 +0600

    classwork 9c

--- server.js
+++ server.js
@@ -1,100 +1,36 @@
-const http = require('http');
-const app = require('express')();
-// const server = http.Server(app);
-// const path = require('path');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const mongo = require('mongodb');
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname+'/index.html');
-});
-
-
-app.get('/second', function(req, res){
-	res.sendFile(__dirname+'/second.html');
-});
-
-app.get('/new_article', function(req, res){
-	res.sendFile(__dirname+'/new_article.html');
-});
-
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.Server(app);
+var bodyParser = require('body-parser')
+//var mongo = require('mongodb')
+var mongoose = require('mongoose')
 
 
 const url = 'mongodb+srv://aak:aak1234@cluster0-n2krw.mongodb.net/test?retryWrites=true&w=majority'
 
-
- 
- 
-mongo.MongoClient.connect(url,{useNewUrlParser: true, useUnifiedTopology: true },
-  function(err, client){
-    if(err){
-      console.log('Could not connect to MongoDB')
-    }else{
-      db = client.db("node-cw9")
-    }
-  })
-
-
-mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true})
-mongoose.connection.on('error',function(err){
-  console.log('could not connect to mongoose')
-})
-// var save = function(form_data){
-  
-// db.createCollection('article',function(err,collection){
-//   var collection = db.collection('article')
-//   collection.save(form_data)
-// })
-// }
-
-var Schema = mongoose.Schema
-var articleSchema = new Schema(
-  {
-    title:{
-      type: String,
-      required: 'title is required'
-    },
-    content:{
-      type: String,
-      required: 'content is required'
-    }
-  }
-)
-var Article = mongoose.model("Article",articleSchema)
-
-app.get('/articles', function(request,response){
-      Article.find(function(err,data){
-        if(err){
-          return response.status(400).json({msg: 'cant find'})
-        }
-        return response.render("article.ejs",{
-          'article': data
-        })
-      })
-    })
-
-
-app.post('/new_article', function(request,response){
-
-  let article = new Article(request.body)
-  article.save(function(err,data){
-    if(err){
-      return  response.status(400).json({msg: "All fields are required"})
-    }
-    return response.status(200).json({article:data})
-
-  })
-  
+mongoose.connect(url, 
+    {useNewUrlParser:true, useUnifiedTopology: true })
+mongoose.connection.on('error', function(err){
+    console.log('Could not connect to MongoDB')
 })
 
 
-   
-app.listen(3000, 'localhost', function() {
+
+app.use(bodyParser.urlencoded({extended:true}))
+require('./routes/article.routes')(app)
+let articles = []
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname+'/views/index.html')
+})
+
+app.get('/second', function(request, response){
+    response.sendFile(__dirname+'/views/second.html')
+})
+
+
+
+server.listen(3000, 'localhost', function(){
     console.log('Server running');
 });
